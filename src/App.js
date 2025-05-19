af677f94c7626780491a13b76ebbca82b614be94
Javeria-Zaheer <41473@students.riphah.edu.pk>
2025-05-19 17:45:47 +0500

    work-done

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div style={{ padding: "20px" }}>
-      <h2>{isLogin ? "Javeria" : "Zaheer"}</h2>
+      <h2>{isLogin ? "Login" : "Register"}</h2>
       <input
         type="email"
         placeholder="Email"
